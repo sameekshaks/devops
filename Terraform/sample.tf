cccf5f2d631d246d4707351e32c76c1e53034e23
Sameeksha <sameekshashitole22@gmail.com>
2025-08-06 20:03:13 +0530

    ec2 instance file

--- Terraform/sample.tf
+++ Terraform/sample.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "myinstance" {
     ami = "ami-0a7d80731ae1b2435"
     instance_type = "t3.micro"
     key_name = "EKS-Key"
-    vpc_security_group_ids = "sg-0943c3a930ba852be"
+    vpc_security_group_ids = ["sg-0943c3a930ba852be"]
   
 }
